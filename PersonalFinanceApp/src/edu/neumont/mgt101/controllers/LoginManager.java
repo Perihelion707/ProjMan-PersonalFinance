6c495f4bab896e1758873f47e1a3d3f434f33bc0
Perihelion <dylanpratt707@gmail.com>
2026-05-18 10:17:04 -0600

    basics of login save/write
    login manager can now write to a text file and read from it

--- PersonalFinanceApp/src/edu/neumont/mgt101/controllers/LoginManager.java
+++ PersonalFinanceApp/src/edu/neumont/mgt101/controllers/LoginManager.java
@@ -2,10 +2,19 @@ package edu.neumont.mgt101.controllers;
 
 import edu.neumont.mgt101.models.User;
 
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import com.fasterxml.jackson.databind.node.ObjectNode;
+import java.io.FileNotFoundException;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class LoginManager {
-    private final String LOGIN_SAVE_PATH = "";
+    private final String LOGIN_SAVE_PATH = "./LoginData.txt";
 
     private ArrayList<User> userAccounts;
     private User currentUser = null;
@@ -24,5 +33,47 @@ public class LoginManager {
 
     public boolean isLoggedIn(){
         return true;
+    }
+
+    public void writeLoginData(){
+
+        File file = new File(LOGIN_SAVE_PATH);
+        if (Files.exists(Paths.get(LOGIN_SAVE_PATH))){
+            System.out.println("Login data has been found successfully.");
+        }
+
+        try{
+            if (file.createNewFile()){
+                System.out.println("Login data has been created successfully.");
+            }else{
+                System.out.println("Login data has been not been created.");
+            }
+        } catch(IOException e){
+            e.printStackTrace();
+            return;
+        }
+
+        try {
+            FileWriter fw = new FileWriter(LOGIN_SAVE_PATH, true);
+            fw.write("\nim just a little guy and this is a little test.");
+            fw.close();
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public String readFile(){
+        File file = new File(LOGIN_SAVE_PATH);
+        StringBuilder content = new StringBuilder();
+        try (Scanner reader = new Scanner(file)) {
+            while (reader.hasNextLine()) {
+                String line = reader.nextLine();
+                content.append(line);
+                System.out.println(line);
+            }
+        } catch (FileNotFoundException fnfe) {
+            fnfe.printStackTrace();
+        }
+        return content.toString();
     }
 }
