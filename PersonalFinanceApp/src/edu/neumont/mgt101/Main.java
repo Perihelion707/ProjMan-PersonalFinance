9d6cb39fd4eeb5c02998f0e851856571168cbf64
CalebDeanMcCool <calebdeanmccool@gmail.com>
2026-05-19 09:57:37 -0600

    Added code to the mainMenu method

--- PersonalFinanceApp/src/edu/neumont/mgt101/Main.java
+++ PersonalFinanceApp/src/edu/neumont/mgt101/Main.java
@@ -1,10 +1,11 @@
 package edu.neumont.mgt101;
 
 
+import edu.neumont.mgt101.controllers.FinanceController;
 import edu.neumont.mgt101.controllers.LoginManager;
 
 public class Main {
     public static void main() {
-     
+        new FinanceController().run();
     }
 }
--- PersonalFinanceApp/src/edu/neumont/mgt101/controllers/FinanceController.java
+++ PersonalFinanceApp/src/edu/neumont/mgt101/controllers/FinanceController.java
@@ -1,14 +1,24 @@
 package edu.neumont.mgt101.controllers;
 
 import edu.neumont.mgt101.models.User;
+import edu.neumont.mgt101.view.TUI;
 
 public class FinanceController
 {
     private User currUser;
 
-    public void run(){}
+    public void run(){
+
+    }
     public void login(){}
-    public void mainMenu(){}
+    public int mainMenu(){
+        TUI.println("1. Deposit Money " +
+                "\n2. Withdraw Money " +
+                "\n3. Add Finance " +
+                "\n4. Add Transaction ");
+        int userChoice = TUI.inputMenuOption("What would you like to do?");
+        return userChoice;
+    }
     public void addGoal(){}
     public void changeIncome(){}
     public void changeExpenses(){}
