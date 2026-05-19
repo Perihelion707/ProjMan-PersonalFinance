114efe873af6a652e29fb5b949ea5361555656b2
davidsonaparker <davidsonaparker@gmail.com>
2026-05-19 09:35:56 -0600

    Add the basic methods in FinanceController.

--- PersonalFinanceApp/src/edu/neumont/mgt101/controllers/FinanceController.java
+++ PersonalFinanceApp/src/edu/neumont/mgt101/controllers/FinanceController.java
@@ -1,4 +1,17 @@
 package edu.neumont.mgt101.controllers;
 
-public class FinanceController {
+import edu.neumont.mgt101.models.User;
+
+public class FinanceController
+{
+    private User currUser;
+
+    public void run(){}
+    public void login(){}
+    public void mainMenu(){}
+    public void addGoal(){}
+    public void changeIncome(){}
+    public void changeExpenses(){}
+    public void addTransaction(){}
+    public void saveAndLogout(){}
 }
