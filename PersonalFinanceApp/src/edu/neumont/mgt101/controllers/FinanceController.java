e528ed22b2f75d58cc740b2b524f365e41ef4219
davidsonaparker <davidsonaparker@gmail.com>
2026-05-19 10:03:21 -0600

    Made the add goal function.

--- PersonalFinanceApp/src/edu/neumont/mgt101/controllers/FinanceController.java
+++ PersonalFinanceApp/src/edu/neumont/mgt101/controllers/FinanceController.java
@@ -1,6 +1,8 @@
 package edu.neumont.mgt101.controllers;
 
+import edu.neumont.mgt101.models.FinancialGoal;
 import edu.neumont.mgt101.models.User;
+import edu.neumont.mgt101.view.TUI;
 
 public class FinanceController
 {
@@ -9,7 +11,34 @@ public class FinanceController
     public void run(){}
     public void login(){}
     public void mainMenu(){}
-    public void addGoal(){}
+    /**
+     * Asks the User for Financial Goal Parameters and then adds that goal to the user
+     */
+    public void addGoal()
+    {
+        while (true)
+        {
+            try {
+                TUI.println("What is the max amount of money you are allowing yourself to spend during this goal");
+                String goalMoney = TUI.inputString();
+                int goalAmount = Integer.parseInt(goalMoney);
+                TUI.println("What do you want this goal to be named");
+                String goalName = TUI.inputString();
+                TUI.println("How many days should this goal last? \n");
+                String goalLength = TUI.inputString();
+                int goalDays = Integer.parseInt(goalLength);
+
+                FinancialGoal userGoal = new FinancialGoal(goalAmount, goalName, goalDays, false);
+                currUser.financialGoals.add(userGoal);
+                break;
+
+            }
+            catch (Exception e)
+            {
+                TUI.println(e.getMessage());
+            }
+        }
+    }
     public void changeIncome(){}
     public void changeExpenses(){}
     public void addTransaction(){}
