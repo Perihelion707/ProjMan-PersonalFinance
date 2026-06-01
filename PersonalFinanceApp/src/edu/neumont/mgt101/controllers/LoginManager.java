9498673e757925374e9d74aaa844f5caab529ca6
davidsonaparker <davidsonaparker@gmail.com>
2026-06-01 09:06:07 -0600

    Changed some stuff idk what.

--- PersonalFinanceApp/src/edu/neumont/mgt101/controllers/LoginManager.java
+++ PersonalFinanceApp/src/edu/neumont/mgt101/controllers/LoginManager.java
@@ -21,6 +21,7 @@ public class LoginManager {
     }
 
     public User login(String username, String password) {
+        userAccounts = FileManager.getUserAccounts();
         for  (User user : userAccounts) {
             if (user.getUserName().equals(username) && user.getPassword().equals(password)) {
                 currentUser = user;
