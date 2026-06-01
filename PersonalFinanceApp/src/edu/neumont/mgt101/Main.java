3f02cb04122e51c42163c162e1bc1508c40a6659
Perihelion <dylanpratt707@gmail.com>
2026-06-01 09:18:16 -0600

    added test printout

--- PersonalFinanceApp/src/edu/neumont/mgt101/Main.java
+++ PersonalFinanceApp/src/edu/neumont/mgt101/Main.java
@@ -1,11 +1,17 @@
 package edu.neumont.mgt101;
 
 
+import edu.neumont.mgt101.controllers.FileManager;
 import edu.neumont.mgt101.controllers.FinanceController;
 import edu.neumont.mgt101.controllers.LoginManager;
 
+import java.io.FilterOutputStream;
+
 public class Main {
     public static void main() {
+        //todo: Remove after testing is over
+        System.out.println(FileManager.readFile("Users"));
         new FinanceController().run();
+
     }
 }
